3820ec5b731244820e2a03d3464dcd25fecf36b9
guywald <guy@guywald.com>
2021-06-14 11:25:04 +0000

    remove comments

--- main.tf
+++ main.tf
@@ -79,12 +79,6 @@ resource "aws_iam_role" "ec2" {
   tags               = module.this.tags
 }
 
-//resource "aws_iam_role_policy" "default" {
-//  name   = "${module.this.id}-eb-default"
-//  role   = aws_iam_role.ec2.id
-//  policy = data.aws_iam_policy_document.extended.json
-//}
-
 resource "aws_iam_role_policy" "default" {
   name   = "custom-metrics"
   role   = aws_iam_role.ec2.id
@@ -109,22 +103,6 @@ resource "aws_iam_role_policy_attachment" "ssm_ec2" {
     create_before_destroy = true
   }
 }
-
-//resource "aws_iam_role_policy_attachment" "ssm_automation" {
-//  role       = aws_iam_role.ec2.name
-//  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonSSMAutomationRole"
-//
-//  lifecycle {
-//    create_before_destroy = true
-//  }
-//}
-
-//# http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/create_deploy_docker.container.console.html
-//# http://docs.aws.amazon.com/AmazonECR/latest/userguide/ecr_managed_policies.html#AmazonEC2ContainerRegistryReadOnly
-//resource "aws_iam_role_policy_attachment" "ecr_readonly" {
-//  role       = aws_iam_role.ec2.name
-//  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-//}
 
 resource "aws_ssm_activation" "ec2" {
   name               = module.this.id
@@ -165,171 +143,6 @@ resource "aws_iam_role_policy" "extended" {
   role   = aws_iam_role.ec2.id
   policy = data.aws_iam_policy_document.custom-extensions.json
 }
-
-//data "aws_iam_policy_document" "default" {
-//  statement {
-//    actions = [
-//      "elasticloadbalancing:DescribeInstanceHealth",
-//      "elasticloadbalancing:DescribeLoadBalancers",
-//      "elasticloadbalancing:DescribeTargetHealth",
-//      "ec2:DescribeInstances",
-//      "ec2:DescribeInstanceStatus",
-//      "ec2:GetConsoleOutput",
-//      "ec2:AssociateAddress",
-//      "ec2:DescribeAddresses",
-//      "ec2:DescribeSecurityGroups",
-//      "sqs:GetQueueAttributes",
-//      "sqs:GetQueueUrl",
-//      "autoscaling:DescribeAutoScalingGroups",
-//      "autoscaling:DescribeAutoScalingInstances",
-//      "autoscaling:DescribeScalingActivities",
-//      "autoscaling:DescribeNotificationConfigurations",
-//    ]
-//
-//    resources = ["*"]
-//
-//    effect = "Allow"
-//  }
-//
-//  statement {
-//    sid = "AllowOperations"
-//
-//    actions = [
-//      "autoscaling:AttachInstances",
-//      "autoscaling:CreateAutoScalingGroup",
-//      "autoscaling:CreateLaunchConfiguration",
-//      "autoscaling:DeleteLaunchConfiguration",
-//      "autoscaling:DeleteAutoScalingGroup",
-//      "autoscaling:DeleteScheduledAction",
-//      "autoscaling:DescribeAccountLimits",
-//      "autoscaling:DescribeAutoScalingGroups",
-//      "autoscaling:DescribeAutoScalingInstances",
-//      "autoscaling:DescribeLaunchConfigurations",
-//      "autoscaling:DescribeLoadBalancers",
-//      "autoscaling:DescribeNotificationConfigurations",
-//      "autoscaling:DescribeScalingActivities",
-//      "autoscaling:DescribeScheduledActions",
-//      "autoscaling:DetachInstances",
-//      "autoscaling:PutScheduledUpdateGroupAction",
-//      "autoscaling:ResumeProcesses",
-//      "autoscaling:SetDesiredCapacity",
-//      "autoscaling:SetInstanceProtection",
-//      "autoscaling:SuspendProcesses",
-//      "autoscaling:TerminateInstanceInAutoScalingGroup",
-//      "autoscaling:UpdateAutoScalingGroup",
-//      "cloudwatch:PutMetricAlarm",
-//      "ec2:AssociateAddress",
-//      "ec2:AllocateAddress",
-//      "ec2:AuthorizeSecurityGroupEgress",
-//      "ec2:AuthorizeSecurityGroupIngress",
-//      "ec2:CreateSecurityGroup",
-//      "ec2:DeleteSecurityGroup",
-//      "ec2:DescribeAccountAttributes",
-//      "ec2:DescribeAddresses",
-//      "ec2:DescribeImages",
-//      "ec2:DescribeInstances",
-//      "ec2:DescribeKeyPairs",
-//      "ec2:DescribeSecurityGroups",
-//      "ec2:DescribeSnapshots",
-//      "ec2:DescribeSubnets",
-//      "ec2:DescribeVpcs",
-//      "ec2:DisassociateAddress",
-//      "ec2:ReleaseAddress",
-//      "ec2:RevokeSecurityGroupEgress",
-//      "ec2:RevokeSecurityGroupIngress",
-//      "ec2:TerminateInstances",
-//      "ecs:CreateCluster",
-//      "ecs:DeleteCluster",
-//      "ecs:DescribeClusters",
-//      "ecs:RegisterTaskDefinition",
-//      "elasticbeanstalk:*",
-//      "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
-//      "elasticloadbalancing:ConfigureHealthCheck",
-//      "elasticloadbalancing:CreateLoadBalancer",
-//      "elasticloadbalancing:DeleteLoadBalancer",
-//      "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-//      "elasticloadbalancing:DescribeInstanceHealth",
-//      "elasticloadbalancing:DescribeLoadBalancers",
-//      "elasticloadbalancing:DescribeTargetHealth",
-//      "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-//      "elasticloadbalancing:DescribeTargetGroups",
-//      "elasticloadbalancing:RegisterTargets",
-//      "elasticloadbalancing:DeregisterTargets",
-//      "iam:ListRoles",
-//      "iam:PassRole",
-//      "logs:CreateLogGroup",
-//      "logs:PutRetentionPolicy",
-//      "rds:DescribeDBEngineVersions",
-//      "rds:DescribeDBInstances",
-//      "rds:DescribeOrderableDBInstanceOptions",
-//      "s3:GetObject",
-//      "s3:GetObjectAcl",
-//      "s3:ListBucket",
-//      "sns:CreateTopic",
-//      "sns:GetTopicAttributes",
-//      "sns:ListSubscriptionsByTopic",
-//      "sns:Subscribe",
-//      "sqs:GetQueueAttributes",
-//      "sqs:GetQueueUrl",
-//      "codebuild:CreateProject",
-//      "codebuild:DeleteProject",
-//      "codebuild:BatchGetBuilds",
-//      "codebuild:StartBuild",
-//    ]
-//
-//    resources = ["*"]
-//
-//    effect = "Allow"
-//  }
-//
-//  statement {
-//    sid = "AllowS3OperationsOnElasticBeanstalkBuckets"
-//
-//    actions = [
-//      "s3:*"
-//    ]
-//
-//    resources = [
-//      "arn:aws:s3:::*"
-//    ]
-//
-//    effect = "Allow"
-//  }
-//
-//  statement {
-//    sid = "AllowDeleteCloudwatchLogGroups"
-//
-//    actions = [
-//      "logs:DeleteLogGroup"
-//    ]
-//
-//    resources = [
-//      "arn:aws:logs:*:*:log-group:/aws/elasticbeanstalk*"
-//    ]
-//
-//    effect = "Allow"
-//  }
-//
-//  statement {
-//    sid = "AllowCloudformationOperationsOnElasticBeanstalkStacks"
-//
-//    actions = [
-//      "cloudformation:*"
-//    ]
-//
-//    resources = [
-//      "arn:aws:cloudformation:*:*:stack/awseb-*",
-//      "arn:aws:cloudformation:*:*:stack/eb-*"
-//    ]
-//
-//    effect = "Allow"
-//  }
-//}
-
-//data "aws_iam_policy_document" "extended" {
-//  source_json   = data.aws_iam_policy_document.default.json
-//  override_json = var.extended_ec2_policy_document
-//}
 
 resource "aws_iam_instance_profile" "ec2" {
   name = "${module.this.id}-eb-ec2"
